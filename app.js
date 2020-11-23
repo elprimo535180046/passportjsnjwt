855713f7ae039ae84140570b30ccd56fa000a235
irvanln <irvina22@gmail.com>
2020-11-23 23:32:36 +0700

    inputdatabase

--- app.js
+++ app.js
@@ -1,35 +1,56 @@
-const express = require('express');
+const express = require("express");
 
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require("express-ejs-layouts");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const flash = require("connect-flash");
+
+const session = require("express-session");
 
 const app = express();
 
 //db config
 
-const db = require('./config/keys').MongoURI;
+const db = require("./config/keys").MongoURI;
 
 //connect mongodb
 
-mongoose.connect(db,{useNewUrlParser:true  , useUnifiedTopology: true  })
-.then(()=> console.log("mongoDB Terkoneksi..."))
-.catch(err => console.log(err));
-
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("mongoDB Terkoneksi..."))
+  .catch((err) => console.log(err));
 
 // ejs
 app.use(expressLayouts);
-app.set('view engine','ejs');
-
+app.set("view engine", "ejs");
 
 //bodyparser
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/',require('./routes/index'));
-app.use('/users',require('./routes/users'));
+// express session middleware
 
+app.use(
+  session({
+    secret: "rahasia",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+//connect flash
+app.use(flash());
+
+// global var
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
+
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port,console.log(`sever started at port ${port}`));
+app.listen(port, console.log(`sever started at port ${port}`));
--- routes/users.js
+++ routes/users.js
@@ -1,43 +1,84 @@
-const express = require('express');
-const router =  express.Router();
+const express = require("express");
+const router = express.Router();
 
+const bcrypt = require("bcryptjs");
 
-router.get('/login',(req,res)=>res.render('login'));
+//user model
+const User = require("../models/User");
 
+router.get("/login", (req, res) => res.render("login"));
 
-router.get('/register',(req,res)=>res.render('register'));
-
+router.get("/register", (req, res) => res.render("register"));
 
 //register post
 
-router.post('/register',(req,res)=>{
-    const {name,email,password,password2} = req.body;
-    // console.log(req.body);
-    // res.send("hello");
-    let errors =[];
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  // console.log(req.body);
+  // res.send("hello");
+  let errors = [];
 
-    //cek required
-    if(!name||!email||!password||!password2){
-        errors.push({msg : 'harap data di input semua'});
-    }
+  //cek required
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "harap data di input semua" });
+  }
 
-    //password 
-    if(password !== password2){
-        errors.push( {msg :'password tidak sama'});
-    }
+  //password
+  if (password !== password2) {
+    errors.push({ msg: "password tidak sama" });
+  }
 
-    if(errors.length>0){
-        res.render('register',{
-            errors,
-        name,
-    email,
-password,
-password2 });
-    }else{
-        res.send('oke lanjut');
-    }
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    //validasi oke lanjut database
+    User.findOne({ email: email }).then((user) => {
+      if (user) {
+        //usernya ada
+        errors.push({ msg: "Email sudah terdaftar" });
+        res.render("register", {
+          errors,
+          name,
+          email,
+          password,
+          password2,
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password,
+        });
+        //hash password
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            //set password jadi hash
+            newUser.password = hash;
 
+            //simpan user
+            newUser
+              .save()
+              .then((user) => {
+                req.flash(
+                  "success_msg",
+                  "Anda berhasil registrasi, Silahkan Login"
+                );
+
+                res.redirect("/users/login");
+              })
+              .catch((err) => console.log(err));
+          })
+        );
+      }
+    });
+  }
 });
-
 
 module.exports = router;
